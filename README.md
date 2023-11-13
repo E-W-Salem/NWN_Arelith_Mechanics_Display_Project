d497a69af3be72e1a151eb468c2d57d368951f47
E-W-Salem <150673340+E-W-Salem@users.noreply.github.com>
2023-11-13 06:08:19 -0500

    Add files via upload
    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Bring in file
+Parse each line and look for keywords
+Retain several different types of lines
+Retain stats for you and monsters
+Attacks and save checks, break it down
+Visualize
+    Graphs
+    Animations
---
+++ process_chat_log.py
@@ -0,0 +1,8 @@
+
+'''
+       Bink _| Base attack | Attack Roll | Critical (Y/N) | Total Damage | Physical Damage | Secondary Damage -all subtypes in own column | 
+
+       Build a class structure for a text file and then parse each line so that if it is a specific format (attack roll, damage output, killed, etc)
+       Each time a specific line is detected it populates a row in a dataframe with specific info
+       This dataframe will be plotted using pandas
+'''
